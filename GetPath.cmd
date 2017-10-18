1c3347373e52fb4130d940cfa08167707a6587ba
Ketchoutchou <32465371+Ketchoutchou@users.noreply.github.com>
2017-10-18 23:18:51 +0200

    Improving launch batch

--- GetPath.cmd
+++ GetPath.cmd
@@ -1,4 +1,3 @@
 @echo off
 echo Running from cmd.exe. Consider using Powershell.exe directly for faster startup
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe %~dp0GetPath.ps1 -FromBatch %*
-REM Will probably need https://blog.netspi.com/15-ways-to-bypass-the-powershell-execution-policy/
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File %~dp0GetPath.ps1 -FromBatch %*
