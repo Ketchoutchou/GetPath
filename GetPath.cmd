33d1b14e38935045b3013d705f20c5155876a45a
Ketchoutchou <32465371+Ketchoutchou@users.noreply.github.com>
2018-04-14 16:47:46 +0200

    Updating cmd to transmit PathExt to the PowerShell runner

--- GetPath.cmd
+++ GetPath.cmd
@@ -1,3 +1,4 @@
 @echo off
 echo Running from cmd.exe. Consider using Powershell.exe directly for faster startup
-C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -File %~dp0GetPath.ps1 -FromBatch %*
+set pwsh=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+%pwsh% -NoProfile -ExecutionPolicy Bypass -File %~dp0%~n0.ps1 -FromBatch -PathExt "%PathExt%" %*
