304990c8e516bb54f830c324361043babcefaeae
Ketchoutchou <32465371+Ketchoutchou@users.noreply.github.com>
2018-04-22 01:44:36 +0200

    Support PowerShell 6

--- GetPath.cmd
+++ GetPath.cmd
@@ -1,4 +1,8 @@
 @echo off
 echo Running from cmd.exe. Consider using Powershell.exe directly for faster startup
-set pwsh=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
-%pwsh% -NoProfile -ExecutionPolicy Bypass -File %~dp0%~n0.ps1 -FromBatch -PathExt "%PathExt%" %*
+if exist "%ProgramW6432%\PowerShell\pwsh.exe" (
+	set pwsh=C:\Program Files\PowerShell\pwsh.exe
+) else (
+	set pwsh=C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+)
+"%pwsh%" -NoProfile -ExecutionPolicy Bypass -File %~dp0%~n0.ps1 -FromBatch -PathExt "%PathExt%" %*
